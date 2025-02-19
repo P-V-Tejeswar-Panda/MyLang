feae0a662493c69117a16ac6a256f93813e463ce
P V Tejeswar Panda <pvtezeswarpanda8@gmail.com>
2025-02-19 23:38:19 +0530

    Flex code complete

---
+++ lexer/lexer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "lexer.h"
+
+extern int yylex();
+extern char* yytext;
+
+int main(int argc, char** argv){
+    int token;
+    while(token = yylex()){
+        std::cout << token;
+        if(token == NUMBER)
+            std::cout << ": " << yytext << std::endl;
+        else if(token == STRING)
+            std::cout << ": " << yytext << std::endl;
+        else if(token == EOF_){
+            std::cout << ": " << yytext << std::endl;
+            break;
+        }
+        else
+            std::cout << ": " << yytext << std::endl;
+    }
+}
